fd70154937140d69cddd0472a52f3a120d055964
virajs <viraj.shelke@merce.co>
2018-09-10 17:06:33 +0530

    Calculator using switch case

---
+++ Calculator/calc_2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+//include<iostream.h> //uncomment if using turbo c++
+//include<conio.h> //uncomment if using turbo c++
+using namespace std;
+
+int main()
+{
+    float a, b;
+    int choice;
+    //clrscr(); //uncomment if using turbo c++
+    cout << "Select any one of the following:" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cin >> choice;
+
+    cout << "Enter two numbers: " << endl;
+    cin >> a >> b;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "The addition of two numbers " << a << " & " << b << " is " << a + b << endl;
+        break;
+    case 2:
+        cout << "The subtraction of two numbers " << a << " & " << b << " is " << a - b << endl;
+        break;
+    case 3:
+        cout << "The multiplication of two numbers " << a << " & " << b << " is " << a * b << endl;
+        break;
+    case 4:
+        if (b == 0)
+        {
+            cout << "Division by zero is not possible!" << endl;
+        }
+        else
+        {
+            cout << "The division of two numbers " << a << " & " << b << " is " << a / b << endl;
+        }
+        break;
+    default:
+        cout << "Invalid Option Selected!";
+    }
+    //getch(); //uncomment if using turbo c++
+    return 0;
+}
